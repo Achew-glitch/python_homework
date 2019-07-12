2c32eea86397024798f81b4445ee5301077a4d12
Achew-Glitch <f9309573N>
2019-07-12 18:47:56 +0300

    Add new_temp

--- Temp.py
+++ Temp.py
@@ -7,7 +7,7 @@ print(c)
 d = 'new test string'
 tuple_string = ('tuple first string')
 print(tuple_string + " + add string")
-print('Hello without goodby')
+print('Hello without goodby;')
 print('Aloha!!!')
-print('Glad to see you')
+print('Glad to see you;')
 new_temp = 'adding new symbols'
