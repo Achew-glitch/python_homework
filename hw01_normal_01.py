3b68d6a825ab875c4f412117aaf0ca35c22204b5
Achew-glitch <artem.kovzik@yandex.ru>
2019-07-23 22:43:49 +0300

    Сделал 1ое задание в домашке normal

--- hw01_normal_01.py
+++ hw01_normal_01.py
@@ -7,3 +7,16 @@
 # * постарайтесь решить задачу с применением арифметики и цикла while;
 # * при желании и понимании решите задачу с применением цикла for.
 
+import random
+
+random_integer = random.randint(1, 1000000)
+print(random_integer)
+max_integer = 0
+
+while random_integer > 0:
+    current_position = int(random_integer % 10)
+    if current_position > max_integer:
+        max_integer = current_position
+    random_integer = int(random_integer / 10)
+
+print('max_integer: ', max_integer)
